038f19940c29f49ca95bf619acd1b192695e5ab7
Sebastian Lazzeri <sebastiano.lazzeri@gmail.com>
2022-10-26 21:09:16 +0200

    apposto

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,63 +2,62 @@ var rightArrow=document.getElementsByClassName('right-arrow');
 var leftArrow=document.getElementsByClassName('left-arrow');
 var play=document.getElementsByClassName('play-pause');
 var cube=document.querySelectorAll('.cube');
-/*var faceFront2=document.querySelectorAll('.cube .front');
-var faceFront3=document.querySelectorAll('.cube .front');*/
 var gradi=0;
 var flag=true;
 var inter; 
+
 rightArrow[0].addEventListener('click',ruotaDx);
 leftArrow[0].addEventListener('click',ruotaSx);
 rightArrow[0].addEventListener('mouseover',ruotaDXHalf);
 leftArrow[0].addEventListener('mouseover',ruotaSXHalf);
-rightArrow[0].addEventListener('mouseout',centra);
-leftArrow[0].addEventListener('mouseout',centra);
+rightArrow[0].addEventListener('mouseout',centraDx);
+leftArrow[0].addEventListener('mouseout',centraSx);
 
 play[0].addEventListener('click',ruotaGiro);
 
-function centra(){
-    cube[0].style.transform="rotateY(0deg)";
-    cube[1].style.transform="rotateY(0deg)";   
-    cube[2].style.transform="rotateY(0deg)";  
+function ruota(){
+    cube[0].style.transform=`rotateY(${gradi}deg)`;
+    cube[1].style.transform=`rotateY(${gradi}deg)`;   
+    cube[2].style.transform=`rotateY(${gradi}deg)`;  
 
 }
+function centraDx(){
+   gradi-=25;
+   ruota();
+
+}
+function centraSx(){
+    gradi+=25;
+    ruota();
+ 
+ }
+ 
 function ruotaDXHalf()
 {
-    cube[0].style.transform="rotateY(25deg)";
-    cube[1].style.transform="rotateY(25deg)";   
-    cube[2].style.transform="rotateY(25deg)";  
+    gradi+=25;
+    ruota();
 }
 function ruotaSXHalf()
 {
-    cube[0].style.transform="rotateY(-25deg)";
-    cube[1].style.transform="rotateY(-25deg)";   
-    cube[2].style.transform="rotateY(-25deg)";  
+    gradi-=25;
+    ruota();
+
 }
 function ruotaDx(){
-   gradi+=90;
-    cube[0].style.transform=`rotateY(${gradi}deg)`;
-    cube[1].style.transform=`rotateY(${gradi}deg)`;   
-    cube[2].style.transform=`rotateY(${gradi}deg)`;
+    gradi+=90;
+    ruota()
 }
 function ruotaSx(){
    gradi-=90;
-    cube[0].style.transform=`rotateY(${gradi}deg)`;
-    cube[1].style.transform=`rotateY(${gradi}deg)`;   
-    cube[2].style.transform=`rotateY(${gradi}deg)`;
+   ruota();
 }
 function ruotaGiro(){
    
    if(flag){
     flag=false;
-    inter=setInterval(giro,1000);
+    inter=setInterval(ruotaDx,1000);
    }
    else clearInterval(inter);
         
         
-}
-function giro(){
-    gradi+=90;
-    cube[0].style.transform=`rotateY(${gradi}deg)`;
-    cube[1].style.transform=`rotateY(${gradi}deg)`;   
-    cube[2].style.transform=`rotateY(${gradi}deg)`;
 }
